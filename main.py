2c528f635cb991112d7e351e15e05dd2cf674751
Anton <alj.boersma2408@gmail.com>
2024-03-27 13:37:59 +0100

    combined MQTT and TCP scripts

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import paho.mqtt.client as mqtt  # make sure to use version 1.6.1
+import paho.mqtt.client as mqtt
 import csv
 from time import time, strftime
 from socket import socket as Socket
 from socket import AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+import threading
 
 # MQTT settings
 mqtt_broker = "aquabots.tech"  # MQTT broker address
@@ -12,7 +13,6 @@ mqtt_client_id = "Anton"  # MQTT client-id
 mqtt_port = 1883  # port used for MQTT broker
 mqtt_keepalive = 60  # duration in [s] before connection is closed
 
-
 topics = [
     "aquastorm/eindmaas/modules/Rudder/0/outputs/button"  # button on rudder module
 ]  # add topics in this list
@@ -20,6 +20,11 @@ topics = [
 # logging to CSV file
 timestamp = strftime("%Y%m%d%H%M%S")
 csv_filename = f"mqtt_data_{timestamp}.csv"
+
+# TCP variables
+HOST = ""  # intentionally left blank, functions as a filter
+PORT = 2345  # port used to communicate over
+message_length = 128  # length of message in ASCII characters
 
 
 # MQTT methods
@@ -50,7 +55,8 @@ def on_message(client, userdata, message):
     #     print(f"Logged: {timestamp}, {topic}, {payload}")
 
 
-def main():
+def mqtt_thread():
+    # Setup MQTT client
     client = mqtt.Client(client_id=mqtt_client_id)
     client.on_connect = on_connect
     client.on_message = on_message
@@ -60,19 +66,45 @@ def main():
 
     client.connect(mqtt_broker, mqtt_port, mqtt_keepalive)
 
-    # Create CSV file with header if it doesn't exist
-    #     with open(csv_filename, mode='a', newline='') as csv_file:
-    #         csv_writer = csv.writer(csv_file)
-    #         csv_writer.writerow(["Timestamp", "Topic", "Value"])
-
     try:
-        while True:
-            client.loop_forever()
+        client.loop_forever()
     except KeyboardInterrupt:
         print("disconnecting")
         client.disconnect()
         exit()
 
 
+def tcp_thread():
+    # Setup TCP server
+    with Socket(AF_INET, SOCK_STREAM) as server_socket:
+        server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        server_socket.bind((HOST, PORT))
+        server_socket.listen()
+        print(f"Listening on port {PORT}")
+        while True:
+            connection, address = server_socket.accept()
+            with connection:
+                print(f"Connected to {address}")
+                while True:
+                    data = connection.recv(message_length)
+                    if not data:
+                        print("Disconnected")
+                        break
+                    content = data.decode("utf-8")
+                    # parts = content.split(":")
+                    # topic = parts[0]
+                    # value = parts[1]
+                    print(f"Received: {content}")
+                    connection.sendall(data)
+                    print("Responded")
+
+
 if __name__ == '__main__':
-    main()
+    mqtt_thread = threading.Thread(target=mqtt_thread)
+    tcp_thread = threading.Thread(target=tcp_thread)
+
+    mqtt_thread.start()
+    tcp_thread.start()
+
+    mqtt_thread.join()
+    tcp_thread.join()
